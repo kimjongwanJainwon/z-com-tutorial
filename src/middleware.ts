e272e652ce581db8f1fab4b90acded2bcd79450d
kjw0810 <kjw0810@jainwon.com>
2024-11-06 23:51:55 +0900

    feature : lint 및 auth middleware

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { auth } from './auth';
+import { NextRequest, NextResponse} from "next/server";
+
+export async function middleware(request: NextRequest) {
+    console.log('middleware' , request.nextUrl.pathname);
+    const session = await auth();
+    if(!session) {
+        return NextResponse.redirect('https://z.nodebird.com/i/flow/login');
+    }
+}
+
+export const config = {
+    matcher: ['/compose/tweet' , '/home' , '/explore' , '/messages' , '/search'],
+}
