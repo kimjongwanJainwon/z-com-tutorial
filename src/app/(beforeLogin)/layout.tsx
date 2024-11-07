5190e5477df83ca42bd9058ce40ace0fd35d43cc
kjw0810 <kjw0810@jainwon.com>
2024-11-08 00:13:10 +0900

    feature : 제에엥발

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -1,6 +1,13 @@
+import {ReactNode} from "react";
+import styles from '@/app/(beforeLogin)/_component/main.module.css';
+
+type Props = {
+    children: ReactNode;
+    modal: ReactNode;
+}
 export default function Layout({children, modal}: { children: React.ReactNode, modal: React.ReactNode }) {
     return (
-        <div>
+        <div className={styles.container}>
             {children}
         </div>
     )
