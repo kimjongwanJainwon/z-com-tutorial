a92320d0d7015edbd2624854242017e50461d5ab
kjw0810 <kjw0810@jainwon.com>
2024-11-07 16:56:34 +0900

    feature : type 재정의

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -1,8 +1,12 @@
 import {ReactNode} from "react";
 import styles from '@/app/(beforeLogin)/_component/main.module.css';
 
-type Props = { children: ReactNode, modal: ReactNode };
-export default function Layout({ children, modal }: Props) {
+interface Props {
+    children: ReactNode;
+    modal: ReactNode;
+}
+
+export default function Layout({children, modal}: Props) {
     return (
         <div className={styles.container}>
             {children}
