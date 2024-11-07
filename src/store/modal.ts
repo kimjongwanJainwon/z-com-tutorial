b963fc43e9ba4a8acf1041dc8707c2bed6302641
kjw0810 <kjw0810@jainwon.com>
2024-11-07 14:52:18 +0900

    feature : 나머지

---
+++ src/store/modal.ts
@@ -0,0 +1,34 @@
+import {Post} from "@/model/Post";
+import {create} from "zustand";
+
+interface ModalState {
+    mode: 'new' | 'comment',
+    data: Post | null,
+
+    setMode(mode: 'new' | 'comment'): void;
+
+    setData(data: Post): void;
+
+    reset(): void;
+}
+
+export const useModalStore = create<ModalState>((set) => ({
+    mode: 'new',
+    data: null,
+    setMode(mode) {
+        set({
+            mode,
+        });
+    },
+    setData(data) {
+        set({
+            data
+        });
+    },
+    reset() {
+        set({
+            data: null,
+            mode: 'new',
+        })
+    }
+}));
